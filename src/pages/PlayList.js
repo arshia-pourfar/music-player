1ac69db79601e1553af859d9ad7daeebae9f9e5c
arshiapourfar <arshiapourfar@gmail.com>
2024-08-22 03:08:36 +0330

    test upload

--- src/pages/PlayList.js
+++ src/pages/PlayList.js
@@ -4,7 +4,7 @@ const PlayList = () => {
     return (
         <section id='play-list-page' className='relative w-full bg-custom-white h-[100dvh] rounded-l-xl py-5 px-10'>
             <h1 className='font-black block text-5xl'>PlayList</h1>
-            
+            mfkdvmdfvm
         </section >
     );
 }
