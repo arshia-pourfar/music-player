849c3bc0bb8277bb51bd7d982d0aa2327a03f89d
arshiapourfar <arshiapourfar@gmail.com>
2025-07-31 14:09:14 +0330

    connect to vercel db

---
+++ src/pages/api/allmusiclist.js
@@ -0,0 +1,21 @@
+// src/lib/api/allmusiclist.js
+const { pool } = require('./db.js'); // اتصال به دیتابیس
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        try {
+            const result = await pool.query('SELECT * FROM allmusiclist');
+            const updatedResult = result.rows.map((item, index) => ({
+                ...item,
+                newId: index
+            }));
+            res.status(200).json(updatedResult);
+        } catch (error) {
+            console.error('Error fetching music list:', error);
+            res.status(500).json({ message: "Internal Server Error" });
+        }
+    } else {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
---
+++ src/pages/api/register.js
@@ -0,0 +1,26 @@
+// src/lib/api/register.js
+const { pool } = require('./db.js'); // اتصال به دیتابیس
+
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        const { username, email, password } = req.body;
+
+        // بررسی صحت ورود اطلاعات
+        if (!username || !email || !password) {
+            return res.status(400).json({ message: 'Username, email, and password are required' });
+        }
+
+        const query = "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)";
+
+        pool.query(query, [username, email, password], (err, result) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
+            res.status(201).json({ message: 'User registered successfully', redirect: '/User' });
+        });
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+}
